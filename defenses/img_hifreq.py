37e92f69ceae4342ef948da3fa35a3fdd2d3317e
Shoi <617555356@qq.com>
2024-02-07 13:31:09 +0800

    DB and hifreq v1

--- defenses/img_hifreq.py
+++ defenses/img_hifreq.py
@@ -1,29 +1,94 @@
 #!/usr/bin/env python3
-# Author: 
+# Author: Shoi
 # Create Time: 2024/02/03
 
+
+import os,sys 
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+sys.path.insert(0,parentdir)  
 from utils import *
+from pywt import dwt2, wavedec2
 
 
-def img_hifreq_by_unsharp_mask(img:PILImage, args:Namespace) -> npimg_hi:
-  pass
+def img_hifreq_by_unsharp_mask(img:PILImage) -> npimg_hi:
+  
+  img_usm = img.filter(ImageFilter.UnsharpMask(2,100,3))
+  img_usm = pil_to_npimg(img_usm)
+  im_raw = pil_to_npimg(img)
+  im_raw_hi = minmax_norm(npimg_diff(img_usm, im_raw))
+  # im_raw_lo = minmax_norm(npimg_diff(im_raw, im_raw_hi))
+
+  # plt.imshow(im_raw_lo)     ; plt.axis('off')
+  # plt.show()
+
+  return im_raw_hi
 
 
-def img_hifreq_by_fft(img:PILImage, args:Namespace) -> npimg_hi:
-  pass
+def img_hifreq_by_fft(img:PILImage) -> npimg_hi:
+  im_raw = np.asarray(img.convert('RGB'), dtype=np.float32)
+  h,w = im_raw.shape[:2]
+  
+  lpf = np.zeros((h,w,3))
+  R = (h+w)//8  
+  for x in range(w):
+      for y in range(h):
+          if ((x-(w-1)/2)**2 + (y-(h-1)/2)**2) < (R**2):
+              lpf[y,x,:] = 1
+  print(lpf)
+  hpf = 1-lpf
+
+  freq = np.fft.fft2(im_raw,axes=(0,1))
+  freq = np.fft.fftshift(freq)
+  lf = freq * lpf
+  hf = freq * hpf
+
+  img_l = np.abs(np.fft.ifft2(lf,axes=(0,1)))
+  img_l = np.clip(img_l,0,255) 
+  img_l = img_l.astype('uint8')
+
+  img_h = np.abs(np.fft.ifft2(hf,axes=(0,1)))
+  img_h = np.clip(img_h,0,255) 
+  img_h = img_h.astype('uint8')
 
 
-def img_hifreq_by_dwt(img:PILImage, args:Namespace) -> npimg_hi:
-  pass
+
+  return img_h
 
 
-def img_hifreq_by(method:str, img:PILImage, args:Namespace):
-  return globals()[f'img_hifreq_by_{method}'](img, args)
+def img_hifreq_by_dwt(img:PILImage) -> npimg_hi:
+  r, g, b = img.split()
+  
+  cA1 = dwt2(r, 'haar')[0]
+  cA2 = dwt2(g, 'haar')[0]
+  cA3 = dwt2(b, 'haar')[0]
+
+  new_size = (224, 224)
+  r_lo = Image.fromarray(cA1).convert('L').resize(new_size)
+  g_lo = Image.fromarray(cA2).convert('L').resize(new_size)
+  b_lo = Image.fromarray(cA3).convert('L').resize(new_size)
+
+  new = Image.merge('RGB', (r_lo, g_lo, b_lo))
+
+  img_lo = pil_to_npimg(new)
+  img_raw = pil_to_npimg(img)
+  img_hi = minmax_norm(npimg_diff(img_raw, img_lo))
+
+  return img_hi
+
+
+def img_hifreq_by(method:str, img:PILImage):
+  return globals()[f'img_hifreq_by_{method}'](img)
 
 
 
 if __name__ == '__main__':
   from plot import *
+  FILE = ".\dataset\ILSVRC2012_val_00000031.png"
+  image = Image.open(FILE)
+  img_h = img_hifreq_by("dwt",image)
 
+  plt.subplot(221) ; plt.imshow(image)     ; plt.axis('off') ; plt.title('X')
+  plt.subplot(222) ; plt.imshow(img_h)     ; plt.axis('off') ; plt.title('AX')
+  plt.tight_layout()
+  plt.show()
   # unitest
-  pass
--- defenses/vector_db.py
+++ defenses/vector_db.py
@@ -1,33 +1,75 @@
 #!/usr/bin/env python3
-# Author: 
+# Author: Shoi
 # Create Time: 2024/02/03 
 
 from __future__ import annotations
 
+import os,sys 
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+sys.path.insert(0,parentdir) 
 from utils import *
-
+import faiss
 
 class VectorDB:
 
-  def __init__(self, dim:int=256, fp:Path=None):
+  def __init__(self, index:faiss.IndexFlatL2=None, dim:int=256, fp:Path=None):
+    self.index = index
     self.dim = dim
     self.fp = fp
 
   @classmethod
   def load(cls, fp:Path) -> VectorDB:
     assert fp.exists()
+    index = faiss.read_index(str(fp))
+    dim = index.d
+    return VectorDB(index, dim, fp)
 
   def save(self, fp:Path=None):
+    # fp = "xxx.index"
     fp = fp or self.fp
+    assert fp != None, "no Path"
+    assert fp.suffix == ".index","xxx.index"
+
+    faiss.write_index(self.index, str(fp))
 
   def add(self, v:ndarray):
     # v.shape == [N, D]
-    assert self.dim != v.shape[-1]
+    assert self.dim == v.shape[-1],"dim error"
+    self.index.add(v)
 
-  def search(self, v:ndarray, k:int=10) -> ndarray:
-    pass
+
+  def search(self, v:ndarray, k:int=10) -> (ndarray,ndarray):
+    assert self.dim == v.shape[-1],"dim error"
+    # list and distance
+    return self.index.search(v, k)
 
 
 if __name__ == '__main__':
+  # data
+  dim = 64
+  data = np.random.rand(500, dim).astype('float32')
+  search_data = np.random.random((5, dim)).astype('float32')
+
+  # init
+  index = faiss.IndexFlatL2(dim) 
+  DB = VectorDB(index, dim)
+
+  # load data
+  # print(Path.cwd()/'defenses/index.index')
+  # DB = VectorDB.load(Path.cwd()/'defenses/index.index')
+
+  # add 
+  DB.add(data)
+  print(DB.index.ntotal)
+
+  # search
+  D,I = DB.search(search_data, 4)
+  print(I)
+  print(D)
+
+  # save
+  # DB.save(Path.cwd()/'defenses/newindex.index')
+  DB.save()
+
   # unitest
   pass
